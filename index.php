9ad89590c66c619ea22596929419ed02fb660ec9
Erik Baalbergen <erikbaalbergen@gmail.com>
2017-02-21 11:24:04 +0100

    Created index.php and added bootstrap.

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta content="nl" http-equiv="Content-Language">
+        <title>TVA divisie Triathlon</title>
+    </head>
+    <body>
+        <h1>TVA divisie Triathlon</h1>
+    </body>
+</html>
