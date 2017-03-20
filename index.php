54b85f2026a2f8b964649584d6ecbfcbd040b900
Erik Baalbergen <erikbaalbergen@gmail.com>
2017-03-20 23:26:53 +0100

    Cleaned up the website, added normalize.css and started on the main HTML

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta content="nl" http-equiv="Content-Language">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="css/normalize.css">
+        <link rel="stylesheet" type="text/css" href="css/main.css">
         <title>TVA divisie Triathlon</title>
     </head>
     <body>
@@ -13,7 +16,56 @@
             echo "<h1>TVA divisie Triathlon</h1><p>Op deze website komt over een korte tijd alles over het divisie-team van TVAlmere. <br><a href=\"http://tvalmere.nl\">Terug naar TVAlmere.nl</a></p>";
         } else
         {
-            
+        ?>
+        <div class="container">
+            <header>
+                <div class="afbeelding">
+                    <img src="http://placehold.it/1400x300">
+                </div>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="./">Home</a>
+                        </li>
+                        <li>
+                            <a href="./?nieuws">Nieuws</a>
+                        </li>
+                        <li>
+                            <a href="./?teams=overzicht">Teams</a>
+                            <ul>
+                                <li>
+                                    <a href="./?teams=eredivisie">Eredivisie</a>
+                                </li>
+                                <li>
+                                    <a href="./?teams=1edivisie">1eDivisie</a>
+                                </li>
+                                <li>
+                                    <a href="./?teams=2edivisie">2eDivisie</a>
+                                </li>
+                                <li>
+                                    <a href="./?teams=3edivisie">3eDivisie</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="./?sponsoren">Sponsoren</a>
+                        </li>
+                        <li>
+                            <a href="http://tvalmere.nl">TV Almere</a>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+            <article>
+            </article>
+            <aside>
+            </aside>
+            <div class="advertenties">
+            </div>
+            <footer>
+            </footer>
+        </div>
+        <?php
         }
         ?>
 
